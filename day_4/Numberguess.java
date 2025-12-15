44258b4f31803244a396c2f96694800efb774460
nabin-bk <nabinbk1255@gmail.com>
2025-12-14 22:12:14 -0800

    update

--- day_4/Numberguess.java
+++ day_4/Numberguess.java
@@ -1,20 +1,42 @@
-import java.util.Scanner;
 
-public class Numberguess {
+public class  Numberguessing {
+   int num;
+   int num2;
 
-   private int secretnumber;
-    
-   Numberguess(int number){
-    Secretnumber=number;
+   Numberguessing(int num){
+    this.num = num;
+
 
    }
-     
-    void check(){
-        Scanner input = new Scanner(System.in);
+   
+   void check(){
+       Scanner input = new Scanner(System.in);
+       
 
+     while(true){
+        System.out.println("Enter the number");
+        num2 = input.nextInt();
 
-    }
+        if(num2>num){
+            System.out.println("Your number " + num2 + " is greater");
+        }
 
-
+        else if(num2<num){
+            System.out.println("Your number " + num2 + " is smaller");
+        }
+      
+      else if(num2 == num){
+         System.out.println("Your guess is correct ");
+        
+      }
      }
+   }
+
+   public static void main(String[] aargs){
+ 
+     Numberguessing ng = new Numberguessing(5);
+     ng.check();
+
+   }
+}
     
