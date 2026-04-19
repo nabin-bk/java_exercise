115a6bd3e76b86c7fa00e0065e0dbe47dd957df0
Nabin Bk <nabinbk1255@gmail.com>
2026-04-19 15:59:14 +0545

    Updated New file

--- day_1/Hello.java
+++ day_1/Hello.java
@@ -1,8 +1,25 @@
-public class hello{
-    
-    public static main(string[] args){
+import java.util.Scanner;
+public class Hello{
+   
+ public static void main(String[] args){
+ Scanner input = new Scanner(System.in);
+    int num1;
+    int num2;
+    int num3;
 
-        
-        system.out.println("Hello");
-    }
+    System.out.println("Enter first your number");
+    num1 = input.nextInt();
+
+     System.out.println("Enter second your number");
+     num2 = input.nextInt();
+
+      System.out.println("Enter third your number");
+      num3= input.nextInt();
+
+
+    int sum=num1+num2+num3;
+    System.out.println("Your number is " + sum);
+
+ }
+    
 }
